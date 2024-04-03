5ba1cbc7d141c62671a581551049097f0cf3cbc2
nilberm <nilber16@outlookcom>
2024-04-03 08:26:52 -0300

    feat: added onFucos on search input

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,10 @@ import Image from "next/image";
 import style from "./style.module.scss";
 import logo from "@/assets/logo.png";
 import maginifier from "@/assets/magnifier.png";
-import { ChangeEvent, useContext, useEffect, useState } from "react";
+import { ChangeEvent, useContext, useEffect, useRef, useState } from "react";
 import { ControlTeamsContext } from "@/context/controlTeamsContext";
 import { ControlTeamsFiltredContext } from "@/context/controlTeamsFiltredContext";
+import { FaSearch } from "react-icons/fa";
 
 interface HeaderProps {}
 
@@ -14,6 +15,9 @@ export default function Header(props: HeaderProps) {
   const { teams } = useContext(ControlTeamsContext);
   const { setTeamsFiltred } = useContext(ControlTeamsFiltredContext);
   const [searchTerm, setSearchTerm] = useState("");
+  const [isInputOnFocus, setIsInputOnFocus] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     const filteredTeams = teams.filter((team) =>
@@ -26,19 +30,34 @@ export default function Header(props: HeaderProps) {
     setSearchTerm(event.target.value);
   };
 
+  const handleSearchClick = () => {
+    if (inputRef) {
+      inputRef?.current?.focus();
+    }
+  };
+
   return (
     <header className={style.header}>
       <div className={style.logo}>
         <Image src={logo} alt="Logo Digital College" />
       </div>
-      <div className={style.inputSearch}>
+      <div
+        className={`${style.inputSearch} ${
+          isInputOnFocus ? style.onFocus : ""
+        }`}
+      >
         <input
+          ref={inputRef}
           type="text"
           className={style.input}
           placeholder="Qual grupo está procurando?"
           onChange={handleInputChange}
+          onFocus={() => setIsInputOnFocus(true)}
+          onBlur={() => setIsInputOnFocus(false)}
         />
-        <Image src={maginifier} alt="search icon" />
+        <button type="button" onClick={() => handleSearchClick()}>
+          <FaSearch />
+        </button>
       </div>
     </header>
   );
